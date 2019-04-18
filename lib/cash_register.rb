b70c31cb1e16baf548e18983a67d9f8a9929078e
Jean-Kenel Dessources <jkenel_23@yahoo.fr>
2019-04-18 01:01:57 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -14,11 +14,11 @@ class CashRegister
     self.total += price * item_qty
     @transaction.push(price)
     
-    # i = item_qty
-    # until i == 0 do
-    #   @all_added_items << title
-    #   i -= 1
-    # end
+    i = item_qty
+    until i == 0 do
+      @all_added_items << title
+      i -= 1
+    end
   end
   
   def apply_discount 
